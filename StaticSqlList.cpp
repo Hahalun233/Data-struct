a696c7aaafb1f48759b6cf3f8ef0634905ad0b4a
Hahalun233 <757988842@qq.com>
2021-03-22 21:37:51 +0800

    hello

---
+++ StaticSqlList.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#define MaxSize 50
+#define ElemType int
+using namespace std;
+
+typedef struct{
+    ElemType data[MaxSize];
+    int length;
+}SqList;
+
+void InitList(SqList &L){
+    for(int i=0;i<MaxSize;i++){
+        L.data[i] = 0;
+    }
+    L.length = 0;
+}
+
+int main(){
+    SqList L;
+    InitList(L);
+    for(int i=0;i<MaxSize;i++)
+    {
+        printf("data[%d]=%d\n",i,L.data[i]);
+    }
+    system("pause");
+    
+}
